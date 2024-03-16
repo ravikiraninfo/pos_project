4f8da9e71794de7892a73b98c448d1b8b358adcf
ridhin <ridhin@ravikiraninfotech.com>
2024-03-16 16:39:42 +0530

    updates

--- custom_product/__manifest__.py
+++ custom_product/__manifest__.py
@@ -20,12 +20,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product'],
+    'depends': ['base','product','sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/product_template_view.xml',
+        'views/sale_order_view.xml'
 
     ],
 
