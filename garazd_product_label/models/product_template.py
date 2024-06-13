dd0204bd307cb0de1ad0200e5e283903e6aa7d87
Tanay2212 <tanay22198@gmail.com>
2024-06-13 12:11:48 +0530

    Added server action method

--- garazd_product_label/models/product_template.py
+++ garazd_product_label/models/product_template.py
@@ -11,3 +11,9 @@ class ProductTemplate(models.Model):
         action = self.env['ir.actions.act_window']._for_xml_id('garazd_product_label.action_print_label_from_template')
         action['context'] = {'default_product_ids': self.ids}
         return action
+    
+    def remove_atts(self):
+        for tmpl in self:
+            aids = tmpl.attribute_line_ids.filtered(lambda att: att.attribute_id.id in (1, 3, 6)).ids
+            for aid in aids:
+                tmpl.attribute_line_ids = [(2, aid)]
